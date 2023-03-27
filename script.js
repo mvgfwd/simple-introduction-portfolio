a11fbd252e5d6961313a730afb4f82ccd829d412
Yonathan S <107821271+mvgfwd@users.noreply.github.com>
2023-03-28 00:36:28 +0700

    Add script.js
    Add uncompleted logic

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("connected");
+
+const submitForm = (e) => {
+  const demo = document.getElementById("demo");
+  const inputFullName = document.getElementById("exampleInputEmail1").value;
+  const inputEmail = document
+    .getElementById("exampleInputEmail12")
+    .value.includes("@");
+  const suksesmengirim = document.getElementById("suksesmengirim");
+
+  demo.style.color = "red";
+
+  function containsNumber(str) {
+    return /\d/.test(str);
+  }
+
+  if (inputFullName.length < 1) {
+    alert("Nama tidak boleh kosong");
+    suksesmengirim.classList.remove("show");
+    return false;
+  }
+  if (containsNumber(inputFullName)) {
+    alert("Nama tidak bisa mengandung angka");
+    suksesmengirim.classList.remove("show");
+    return false;
+  }
+
+  if (!inputEmail) {
+    alert("Masukkan Email dengan Benar");
+    return false;
+  }
+
+  console.log("harusnya gak kesini");
+
+  console.log("submitted");
+};
